913c414b536c652cd383654d7a516a2e359a866c
JackNicolus <u6210195@au.edu>
2023-02-01 15:52:47 +0700

    try

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function AboutPage() {
+  return (
+    <>
+    <Head>
+      <title> Jack's About Page</title>
+      </Head>
+      <h1> About </h1>
+      <p>
+        This is about page for Jack
+      </p>
+      </>
+  )
+}
